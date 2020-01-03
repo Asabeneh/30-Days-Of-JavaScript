1c36d5a61e44f0bacd6a707873a5d5c7b0bf600f
tahirs95 <tahirs95@hotmail.com>
2020-01-03 20:34:21 +0500

    Done with Day 1 exercise.

---
+++ 01-Day-Sol/datatypes.js
@@ -0,0 +1,38 @@
+let string_var = "My name is Tahir";
+let bool_var = true;
+let undef_var = undefined
+let null_var = null;
+
+console.log(typeof(string_var))
+console.log(typeof(bool_var))
+console.log(typeof(undef_var))
+console.log(typeof(null_var))
+
+let var1;
+let var2;
+let var3;
+let var4;
+
+let intro = "My name is Tahir";
+let nationality = "Pakistani";
+let married = false;
+let height = 5.10
+
+let first_name = "Muhammad";
+let last_name = "Tahir";
+let marital_status = false;
+let country = "Pakistan";
+let age = 24; 
+
+let _first_name = "Muhammad",
+    _last_name = "Tahir",
+    _marital_status = false,
+    _country = "Pakistan",
+    _age = 24; 
+
+let myAge = 24,
+    yourAge = 30;
+console.log(`I am ${myAge} years old.`)
+console.log(`You are ${yourAge} years old.`)
+
+
---
+++ 01-Day-Sol/variable.js
@@ -0,0 +1,8 @@
+// Comments can make code readable
+// Welcome to 30Days of JavaScript
+/* comments can make code readable, easy to use and informative */
+
+let string_var = "My name is Tahir";
+let bool_var = true;
+let undef_var = undefined
+let null_var = null;
