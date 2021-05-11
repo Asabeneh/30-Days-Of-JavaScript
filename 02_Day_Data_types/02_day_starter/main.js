c112fcc5a08e1a3741e90c482512915a4e613806
matt vile <matt.vile@yahoo.co.uk>
2021-05-11 17:32:23 +0100

    challenge one complete

--- 02_Day_Data_types/02_day_starter/main.js
+++ 02_Day_Data_types/02_day_starter/main.js
@@ -1,1 +1,31 @@
 // this is your main.js script
+let challenge = '30 Days Of JavaScript'
+console.log(challenge)
+console.log(challenge.length)
+console.log(challenge.toUpperCase())
+console.log(challenge.toLowerCase())
+console.log(challenge.substr(3, 4))
+console.log(challenge.substring(3))
+console.log(challenge.includes("script"))
+console.log(challenge.includes("Script"))
+console.log(challenge.split())
+console.log(challenge.split(" "))
+let bigCorps = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon"
+console.log(bigCorps.split(","))
+console.log(challenge.replace("JavaScript", "Python"))
+console.log(challenge.charAt(15))
+console.log(challenge.charCodeAt("j"))
+console.log(challenge.charCodeAt("J"))
+console.log(challenge.indexOf("a"))
+console.log(challenge.lastIndexOf("a"))
+let sillySentence = "You cannot end a sentence with because because because is a conjunction"
+console.log(sillySentence.indexOf("because"))
+console.log(sillySentence.search("because"))
+console.log(sillySentence.lastIndexOf("because"))
+console.log(challenge.trim())
+console.log(challenge.startsWith("30"))
+console.log(challenge.endsWith("JavaScript"))
+console.log(challenge.match("a"))
+let thirty = "30"
+console.log(thirty.concat(' Days of', ' JavaScript'))
+console.log(challenge.repeat(2))
