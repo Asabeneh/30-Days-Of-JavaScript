c3c186987c8d6176f73e549b0738a8e175f72524
Ashley Casimir <ashcas@me.com>
2021-09-06 20:46:02 -0700

    Day 1 excerises done

---
+++ 01_Day_Introduction/01_day_starter/myDatatypes.js
@@ -0,0 +1,15 @@
+console.log(typeof first);
+console.log(typeof last);
+console.log(typeof numbers);
+console.log(typeof inLove);
+console.log(typeof isNull);
+
+let door;
+let car;
+let foot;
+let dog;
+
+// let door = "open";
+// let car = "fast";
+// let foot = 2;
+// const dog = "Chimi";
---
+++ 01_Day_Introduction/01_day_starter/myVariable.js
@@ -0,0 +1,24 @@
+// Comments can make code readable
+// Welcome to 30DaysOfJavaScript
+
+/* 
+    commentts can make code readable, easy to reuse and informative
+*/
+
+// let first = "Ashley";
+// let last = "Casimir";
+// let numbers = 34;
+// let inLove = true;
+// let country = "USA";
+
+let first = "Ashley",
+  last = "Casimir",
+  numbers = 34,
+  inLove = true,
+  home = "USA";
+
+let myAge = 34,
+  yourAge = 27;
+
+console.log(`I am ${myAge} years old`);
+console.log(`you are ${yourAge} years old`);
