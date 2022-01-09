49d888906ee0a1abd37827c8245a116bbeed2b61
Orkhan <64924921+orkhannajafli@users.noreply.github.com>
2022-01-10 02:31:21 +0400

    starts -> ends
    The endsWith() method returns true if a string ends with a specified string. Otherwise it returns false.

--- 02_Day_Data_types/string_methods/ends_with.js
+++ 02_Day_Data_types/string_methods/ends_with.js
@@ -1,4 +1,4 @@
-// endsWith: it takes a substring as an argument and it checks if the string starts with that specified substring. It returns a boolean(true or false).
+// endsWith: it takes a substring as an argument and it checks if the string ends with that specified substring. It returns a boolean(true or false).
 // string.endsWith(substring)
 let string = 'Love is the best to in this world'
 console.log(string.endsWith('world')) // true
