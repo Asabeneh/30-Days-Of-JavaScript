50c30655d777ab068d912a08a931fd3a748d06b6
MumuNiMochii <deanharoldpabad@gmail.com>
2022-01-11 23:10:12 +0800

    commit on 'answers'

---
+++ 01_Day_Introduction/Answers/01_Scripts/04_Multiple_External_Scripts/main.js
@@ -0,0 +1,1 @@
+console.log('This is the main script.')
---
+++ 01_Day_Introduction/Answers/04_Exercises/variables.js
@@ -0,0 +1,13 @@
+let negativeInteger = -10
+    zeroInteger = 0,
+    positiveInteger = 42,
+    negativeDecimal = -16.84,
+    zeroDecimal = 0.0,
+    positiveDecimal = 420.69,
+    singleQuote = 'A string between single quotes',
+    doubleQuote = "A string between double quotes",
+    backtick = `A string between backticks`,
+    trueBool = true,
+    falseBool = false,
+    undefinedVariable = undefined,
+    emptyVariable = null
