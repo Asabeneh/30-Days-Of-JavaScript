9114f0bc43ceacded5845b858cadfedc39651330
Jose Linardo <joselinardo01@gmail.com>
2022-04-15 18:07:12 -0500

    Prueba de commit

--- 05_Day_Arrays/05_day_starter/scripts/main.js
+++ 05_Day_Arrays/05_day_starter/scripts/main.js
@@ -1,3 +1,5 @@
 console.log(countries)
 alert('Open the browser console whenever you work on JavaScript')
 alert('Open the console and check if the countries has been loaded')
+
+fañsdfñlsaf aldfflka jdlkfaflk
