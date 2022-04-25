39578fce0ba880252e90d312106d2f67b71189be
pradeep@marensolutions.com <pradeep@marensolutions.com>
2022-04-25 11:14:49 +0530

    added comment

--- 02_Day_Data_types/string_methods/char_code_at.js
+++ 02_Day_Data_types/string_methods/char_code_at.js
@@ -1,6 +1,6 @@
 // charCodeAt(): Takes index and it returns char code(ASCII number) of the value at that index
 
-string.charCodeAt(index)
+// string.charCodeAt(index)
 let string = '30 Days Of JavaScript'
 console.log(string.charCodeAt(3)) // D ASCII number is 51
 let lastIndex = string.length - 1
--- 02_Day_Data_types/string_methods/index_of.js
+++ 02_Day_Data_types/string_methods/index_of.js
@@ -1,6 +1,6 @@
 // indexOf(): Takes takes a substring and if the substring exists in a string it returns the first position of the substring if does not exist it returns -1
 
-string.indexOf(substring)
+// string.indexOf(substring)
 let string = '30 Days Of JavaScript'
 console.log(string.indexOf('D'))          // 3
 console.log(string.indexOf('Days'))       // 3
