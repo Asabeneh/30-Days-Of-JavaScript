047f3d48778b23848f940d1bf612daf5b75f0d25
fazzy12 <kaluifeanyi00@gmail.com>
2022-07-25 14:22:18 +0100

    boolean operators

--- 03_Day_Booleans_operators_date/03_day_starter/scripts/main.js
+++ 03_Day_Booleans_operators_date/03_day_starter/scripts/main.js
@@ -80,15 +80,15 @@ console.log(date.getMinutes());
 console.log(date.getMinutes());
 
 
-level 2
+// level 2
 
-number 1
+// number 1
 let base = window.prompt('Enter base:');
 let height = window.prompt('Enter height:');
 let result = (base * height) / 2;
 window.alert(`The area of the triangle is ${result}`);
 
-number 2
+// number 2
 
 let side_a = window.prompt('Enter side a:');
 let side_b = window.prompt('Enter side b:');
@@ -97,7 +97,7 @@ let side_c = window.prompt('Enter side c:');
 let parameter = side_a  + side_b  + side_c;
 window.alert(`The perimeter of the triangle is ${parameter}`);
 
-number 3
+// number 3
 
 
 
