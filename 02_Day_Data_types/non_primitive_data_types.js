8e4ce6ab13bcfd9e9ca5d3eb96796cc177aec88e
Zack Dx <harshitmishra@HARSHITs-MacBook-Air.local>
2022-08-30 11:43:16 +0530

    Fixed Blocked-Scope Variables Code Error

--- 02_Day_Data_types/non_primitive_data_types.js
+++ 02_Day_Data_types/non_primitive_data_types.js
@@ -1,10 +1,10 @@
 let nums = [1, 2, 3]
-nums[0] = 10
+nums[0]=10
 console.log(nums) // [10, 2, 3]
 
-let nums = [1, 2, 3]
+let nums1 = [1, 2, 3]
 let numbers = [1, 2, 3]
-console.log(nums == numbers) // false
+console.log(nums1 == numbers) // false
 
 let userOne = {
   name: 'Asabeneh',
@@ -18,13 +18,19 @@ let userTwo = {
 }
 console.log(userOne == userTwo) // false
 
-let numbers = nums
-console.log(nums == numbers)  // true
+let numbers2 = nums
+console.log(nums == numbers2)  // true
 
-let userOne = {
+let userThird = {
 name:'Asabeneh',
 role:'teaching',
 country:'Finland'
 }
-let userTwo = userOne
-console.log(userOne == userTwo)  // true
+
+var  userFourth = {
+  name:'Asabeneh',
+  role:'teaching',
+  country:'Finland'
+}
+ userFourth = userThird
+console.log(userFourth == userThird)  // true
