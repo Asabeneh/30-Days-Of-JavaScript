cb2101178bae32c273e2cf62f52d4ec746c34058
testerUse21 <106081205+testerUse21@users.noreply.github.com>
2022-09-26 14:16:13 +0530

    Create file.js

---
+++ file.js
@@ -0,0 +1,28 @@
+var a;
+a = 5 ;
+var b = 10; 
+console.log ( " test wb checkk " ) ; 
+console.log ( " test again 5 " ) ;
+console.log ( " test again 2 " ) ;
+ console.log ( " test again 3 " );
+ console.log ( " file check " , a );
+ console.log ( a ) ; 
+ debugger ;
+ 
+ Switch ( value ) {
+ case ' 1 ' : 
+       console.log ( " 1 " ) ;
+       break ; 
+ case ' 2 ' : 
+   switch [ val ) {
+      case ' a ' : 
+        console.log ( " nested switch " ) ; 
+		break ; 
+		default : 
+		break ; 
+     }
+	 default:
+		console.log ( " done " ) ;
+         break ;
+		console.log ( " file check " , a ) 
+		}
