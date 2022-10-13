4664e8c66d4b0545fb1583ac58b0977d01cebf9f
Ashu Baghel <102874744+ashu2206@users.noreply.github.com>
2022-10-13 19:57:59 +0530

    Update variable.js

--- 01_Day_Introduction/variable.js
+++ 01_Day_Introduction/variable.js
@@ -1,17 +1,17 @@
 // Declaring different variables of different data types
-let firstName = 'Asabeneh' // first name of a person
-let lastName = 'Yetayeh' // last name of a person
-let country = 'Finland' // country
-let city = 'Helsinki' // capital city
-let age = 100 // age in years
-let isMarried = true
+let firstName = "Ashu"; // first name of a person
+let lastName = "Baghel"; // last name of a person
+let country = "India"; // country
+let city = "Agra"; // capital city
+let age = 21; // age in years
+let isMarried = false;
 
 // Declaring variables with number values
-const gravity = 9.81 // earth gravity  in m/s2
-const boilingPoint = 100 // water boiling point, temperature in oC
-const PI = 3.14 // geometrical constant
+const gravity = 9.81; // earth gravity  in m/s2
+const boilingPoint = 100; // water boiling point, temperature in oC
+const PI = 3.14; // geometrical constant
 
 // Variables can also be declaring in one line separated by comma
-let name = 'Asabeneh', //name of a person
-  job = 'teacher',
-  live = 'Finland'
+let name = "Ashu", //name of a person
+  job = "teacher",
+  live = "India"; 'Finland'
