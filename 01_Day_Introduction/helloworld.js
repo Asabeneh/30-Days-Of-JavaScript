d3901a4b105b9828e6b0278a6dcc757c0190c5f5
Raagul Vignesh R <94353294+RaagulVigneshR@users.noreply.github.com>
2022-12-02 17:31:05 +0530

    Create helloworld.js

---
+++ 01_Day_Introduction/helloworld.js
@@ -0,0 +1,16 @@
+function processData(inputString) {
+    console.log("Hello, World.");
+    console.log(inputString);
+}
+
+
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+_input = "";
+process.stdin.on("data", function (input) {
+    _input += input;
+});
+
+process.stdin.on("end", function () {
+   processData(_input);
+});
