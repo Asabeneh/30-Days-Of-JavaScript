78240d786486e21ff9277f64394c6bbdf1941396
loic242 <bmavah@gmail.com>
2022-12-05 00:10:49 +0200

    Trynna run the array

--- 01_Day_Introduction/01_day_starter/helloworld.js
+++ 01_Day_Introduction/01_day_starter/helloworld.js
@@ -1,1 +1,2 @@
 console.log('Hello, World!')
+//Print something with JS code.
--- 05_Day_Arrays/05_day_starter/scripts/main.js
+++ 05_Day_Arrays/05_day_starter/scripts/main.js
@@ -1,3 +1,26 @@
-console.log(countries)
-alert('Open the browser console whenever you work on JavaScript')
-alert('Open the console and check if the countries has been loaded')
+const numbers = [0, 3.14, 9.81, 37, 98.6, 100] // array of numbers
+const fruits = ['banana', 'orange', 'mango', 'lemon'] // array of strings, fruits
+const vegetables = ['Tomato', 'Potato', 'Cabbage', 'Onion', 'Carrot'] // array of strings, vegetables
+const animalProducts = ['milk', 'meat', 'butter', 'yoghurt'] // array of strings, products
+const webTechs = ['HTML', 'CSS', 'JS', 'React', 'Redux', 'Node', 'MongDB'] // array of web technologies
+const countrie = ['Finland', 'Denmark', 'Sweden', 'Norway', 'Iceland'] // array of strings, countries
+
+// Print the array and its length
+
+console.log('Numbers:', numbers)
+console.log('Number of numbers:', numbers.length)
+
+console.log('Fruits:', fruits)
+console.log('Number of fruits:', fruits.length)
+
+console.log('Vegetables:', vegetables)
+console.log('Number of vegetables:', vegetables.length)
+
+console.log('Animal products:', animalProducts)
+console.log('Number of animal products:', animalProducts.length)
+
+console.log('Web technologies:', webTechs)
+console.log('Number of web technologies:', webTechs.length)
+
+console.log('Countries:', countrie)
+console.log('Number of countries:', countrie.length)
