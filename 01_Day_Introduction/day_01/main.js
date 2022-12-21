eca895b077c36bc7c6c6fd2216ad1a27c51f24a3
Manojreddykatta <manojkatta97@gmail.com>
2022-12-21 07:57:19 +0530

    external scripts linking - day 01

---
+++ 01_Day_Introduction/day_01/main.js
@@ -0,0 +1,11 @@
+console.log("Firstname is "+ firstName ,+ "Lastname is "+ lastName );
+console.log("Age is " + age );
+
+
+let maritualStatus = isMarried;
+if (isMarried != true )
+    {console.log("Unmarried");}
+else
+    {console.log("Married");}
+
+console.log("Maritual stastus" + maritualStatus);
