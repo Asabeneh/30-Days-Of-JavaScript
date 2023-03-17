fe956a5c62064a6c70adbccfc0badc716d088146
Norman Harold Bernales <mhinenwebe9@yahoo.com>
2023-03-17 23:48:49 +0800

    Adding Variable.js Declaring types,number values

---
+++ 01_Day_Introduction/1_day_of_javascript/variable.js
@@ -0,0 +1,19 @@
+//Declaring different variables of different Data types.
+
+let firstName = 'Norman';    //First name of a person.
+let lastName = 'Taz';        //Last name of a person.
+let country = 'Philippines'; //Name of a country.
+let city = 'Manila';         //Name of a city.
+let age = 13;                //Age for years.
+let single = true;           //Status of the person.
+
+
+//Declaring values with number values.
+
+const gravity = 9.81;        //Earth Gravitiy in m/s2.
+const boilingPoint = 100;    //Water boiling point, temperature in oC.
+const PI = 4.14              //The number π is a mathematical constant that is the ratio of a circle's circumference to its diameter, approximately equal to 3.14159.
+
+//Variables can also be declared in one line separated by comma.
+
+let name = 'Kyrie', job = 'Player', live = 'Australia';
