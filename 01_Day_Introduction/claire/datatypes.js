477ffc64f2211c5f5874f4460b867dfd947fd535
cgjj20 <clairejoseph101@gmail.com>
2023-07-21 19:22:41 +0000

    adding day 1 assignment files

---
+++ 01_Day_Introduction/claire/datatypes.js
@@ -0,0 +1,30 @@
+console.log(typeof 10);
+console.log(typeof 'road')
+console.log(typeof false)
+console.log(typeof [cakes])
+
+let signs = ""
+let question = null
+let rivers = {}
+let flowers = []
+
+const question = 'Where is it?'
+const baskets = 10
+let teams = [12, 20, 15, 18]
+const trucks = {color: "blue", size: "heavy duty", use: "logging"}
+
+const name = {
+firstname: "Claire",
+lastname: "Joseph",
+marital status: "single",
+country: "Nigeria",
+age: 26
+};
+
+
+let firstname = "Claire", lastname = "Joseph", marital status = "single", country = "Nigeria", age = 26;
+
+const myAge = 26
+const yourAge = 27
+
+console.log(myAge, yourAge);
---
+++ 01_Day_Introduction/claire/variable.js
@@ -0,0 +1,10 @@
+//comments can make code readable
+//welcome to 30DaysOfjavaScript
+/*comments can make code readable,
+easy to reuse and informative 
+*/
+
+let story = "Noah"
+let bet = true
+let tree;
+let plant = 0
