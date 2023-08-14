7de92b8af3720ee43e0a71ecf5e98c73f4228143
Gideon-Buba <bubaambore@gmail.com>
2023-08-14 23:22:44 +0100

    Day 2: completed challenge

--- 02_Day_Data_types/02_day_starter/main.js
+++ 02_Day_Data_types/02_day_starter/main.js
@@ -1,1 +1,91 @@
 // this is your main.js script
+
+//Excercises for Day 2
+//Exercise 1
+let challenge = '30 Days Of JavaScript';
+
+//Exercise 2
+console.log(challenge)
+
+//Exercise 3
+console.log(challenge.length)
+
+//Exercise 4
+console.log(challenge.toUpperCase())
+
+//Exercise 5
+console.log(challenge.toLocaleLowerCase())
+
+//Exercise 6
+console.log(challenge.substring(2, 8))
+
+//Exercise 7
+console.log(challenge.substring(0, 2))
+
+//Exercise 8
+console.log(challenge.includes('Script'))
+
+//Exercise 9
+console.log(challenge.split())
+
+//Exercise 10
+console.log(challenge.split(' '))
+
+//Exercise 11
+let companies = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon'
+console.log(companies.split(','))
+
+//Exercise 12
+console.log(challenge.replace('JavaScript', 'Python'))
+
+//Exercise 13
+console.log(challenge.charAt(15))
+
+//Exercise 14
+console.log(challenge.charCodeAt(8))
+
+//Exercise 15
+console.log(challenge.indexOf('a'));
+
+//Exercise 16
+console.log(challenge.lastIndexOf('a'));
+
+//Exercise 17
+let sentence = 'You cannot end a sentence with because because because is a conjunction';
+console.log(sentence.indexOf('because'))
+
+//Exercise 18
+console.log(sentence.lastIndexOf('because'))
+
+//Exercise 19
+console.log(sentence.search('because'))
+
+//Exercise 20
+let whiteSpace = ' 30 days of JavaScript ';
+console.log(whiteSpace.trim())
+
+//Exercise 21
+console.log(challenge.startsWith('30'))
+
+//Exercise 22
+console.log(challenge.endsWith('JavaScript'))
+
+//Exercise 23
+console.log(challenge.match())
+
+//Exercise 24
+const firstString = '30 Days of ';
+const secondString = 'JavaScript';
+
+const mergedString = firstString.concat(secondString);
+
+console.log(mergedString); 
+
+//Exercise 25
+console.log(challenge.repeat(2))
+
+
+
+                        //Exercise Level 2
+
+    
