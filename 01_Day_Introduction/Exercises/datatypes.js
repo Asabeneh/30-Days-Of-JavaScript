61b85120139220bdc87c3058da44f3e8a63bdab4
Brendan Klein <deliveritthere@gmail.com>
2023-08-21 20:49:18 -0400

    Completed Day 1 Exercises
    chore: Finished 30 Day of JavaScript Day 1 Exercises

---
+++ 01_Day_Introduction/Exercises/datatypes.js
@@ -0,0 +1,9 @@
+//Exercise 4
+/*
+console.log("myFirstName: "+ typeof myFirstName);
+console.log("myLastName: "+ typeof myLastName);
+console.log("notFound: "+ typeof notFound);
+console.log("myFalseBool: "+ typeof myFalseBool);
+console.log("myTrueBool: "+ typeof myTrueBool);
+console.log("myNullVar: "+ typeof myNullVar);
+*/
---
+++ 01_Day_Introduction/Exercises/variable.js
@@ -0,0 +1,35 @@
+//Exercise 4 and 5
+let myFirstName = 'Brendan'; //My First Name
+let myLastName = 'Klein';    //My Last Name
+let notFound = undefined;   //Random undefined variable
+let myFalseBool = false;    //False boolean
+let myTrueBool = true;      //True boolean
+let myNullVar = null;       //Random null boolean
+
+//Exercise 6
+let undeclaredVariable;
+let undeclaredVariable2;
+let undeclaredVariable3;
+let undeclaredVariable4;
+
+//Exercise 7
+let myString = "coffee";
+let favoriteCodeEditor = "Visual Studio Code";
+let favoriteColor = "Orange";
+let coffeeLover = true;
+
+//Exercise 8
+let myFirstName8 = 'Brendan'; //My First Name
+let myLastName8 = 'Klein';    //My Last Name
+let maritalStatus8 = "single";
+let country8 = 'USA';
+let age8 = 23;
+
+//Exercise 9
+let myFirstName9 = 'Brendan', myLastName9 = 'Klein', maritalStatus9 = "single", country9 = 'USA', age9 = 23;
+
+let myAge="23";
+let yourAge="26";
+
+console.log("I am "+myAge+" years old.");
+console.log("You are "+yourAge+" years old.");
