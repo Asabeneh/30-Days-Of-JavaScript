243607df8f7a7cc7c8e32d4774f838d3bede76af
Dzykas <elegy.dzikas@gmail.com>
2023-08-26 02:48:18 +0300

    exercises for day 1

---
+++ solutions/day-01/datatypes.js
@@ -0,0 +1,9 @@
+// comments can make code readable
+// Welcome to 30DaysOfJavaScript
+/* comments can make code readable,
+easy to reuse and informative */
+
+console.log(typeof("string"));
+console.log(typeof(true));
+console.log(typeof(null));
+console.log(typeof(undefined));
---
+++ solutions/day-01/variable.js
@@ -0,0 +1,16 @@
+let a = "string";
+let b = true;
+let c = null;
+let d = undefined;
+
+let firstName = "Eligijus";
+let lastName = "Dzikavicius";
+let isMarried = false;
+let age = 17;
+let live = "Lithuania"
+
+let myAge = 17;
+let yourAge = 30;
+
+console.log("I am " + myAge + " years old.");
+console.log("You are " + yourAge + " years old.");
