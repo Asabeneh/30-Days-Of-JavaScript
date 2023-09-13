00ec649ea947098b298981d523c11a327a1c233b
Dzykas <elegy.dzikas@gmail.com>
2023-09-13 22:26:29 +0300

    ignas

---
+++ solutions/day-05/data/countries.js
@@ -0,0 +1,13 @@
+const countries = [
+    'Albania',
+    'Bolivia',
+    'Canada',
+    'Denmark',
+    'Ethiopia',
+    'Finland',
+    'Germany',
+    'Hungary',
+    'Ireland',
+    'Japan',
+    'Kenya'
+  ]
---
+++ solutions/day-05/main.js
@@ -0,0 +1,8 @@
+const emptArr = [];
+const arr = [1,2,3,4,5];
+const arrLen = arr.length;
+
+console.log("length is :", arrLen); // array length
+
+console.log(arr[0]); // first item
+console.log([arr.length - 1]); // last item
