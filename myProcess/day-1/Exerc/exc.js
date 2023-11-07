7027943873c37fd06e1c3b73cb3c3762c5de9402
M0lt0 <bodymgahed@yahoo.com>
2023-11-07 06:34:31 -0300

    first les

---
+++ myProcess/day-1/Exerc/exc.js
@@ -0,0 +1,33 @@
+// comment make code more readable
+// welcome js world
+/*comments can make code readable,
+easy to reuse and informative*/
+
+let first = 'javaScript';
+let strongLang = true;
+let undf;
+let random = null;
+
+// four var without value
+let four;
+let three;
+let tow ;
+let one ;
+
+// four var with vales
+let hour = 6;
+let story= 'there is a person that will achieve his goals ';
+let towHundred = 200;
+let onePerson = 'molto' ;
+
+// four var for me
+
+let first_name = 'molto';
+let last_name = 'michele'
+let marital_status = false;
+let count = 'brazil';let age =25;
+
+// last two var
+let myAge = 25;
+let yourAge = 35;
+console.log(`hi i am ${myAge} years old and you are ${yourAge} years old `)
