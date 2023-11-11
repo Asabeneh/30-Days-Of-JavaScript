3ca2716af5bdecc5f242e87a2626f691bb859462
Nevzat Atalay <nevzatatatalay79@gmail.com>
2023-11-11 14:15:21 +0300

    first day file uploated

---
+++ exercise-ansver-key/day1_introdiction.js
@@ -0,0 +1,63 @@
+//---------------------------day1_level_1 1.exercise ------------------------\\
+
+// comments can make code readable
+
+//---------------------------day1_level_1 2.exercise-------------------------\\
+
+//Welcome to 30DaysOfJavaScript
+
+//---------------------------day1_level_1 3.exercise-------------------------\\
+
+/*        comments can make code readable,
+           easy to reuse and informative  */
+
+//---------------------------day1_level_1 4.exercise-------------------------\\
+
+//variable.js
+let string = 'nevzat'
+let number = 25
+let boolean = true
+let nulll = null
+let any;     //undefined
+
+//---------------------------day1_level_1 5.exercise-------------------------\\
+
+//type.js
+console.log(typeof (string) )
+console.log(typeof (number))
+console.log(typeof (boolean))
+console.log(typeof (nulll))
+console.log(typeof (any))
+
+//---------------------------day1_level_1 6.exercise-------------------------\\
+
+    let variable1;
+    let variable2;
+    var variable3;
+    let variable4;
+
+//---------------------------day1_level_1 7. exercise------------------------\\
+
+variable1 = "nevzat"
+variable2 = "ATALAY"
+variable3 =  true;
+variable4 = 25;
+
+//---------------------------day1_level_1 8.exercise-------------------------\\
+
+let firstName = "nevzat"
+let lastName = "atalay"
+let old = 25
+let isMarried = true
+
+//---------------------------day1_level_1 9.exercise-------------------------\\
+
+let name = "nevzat",surName="atalay",age = 25, married = true
+
+//---------------------------day1_level_1 10.exercise------------------------\\
+
+myAge= 25
+let yourAge = 22
+
+console.log("I am " + " " +  myAge + " " + "years old.")
+console.log("You are" + " " +  yourAge + " " +  "years old.")
