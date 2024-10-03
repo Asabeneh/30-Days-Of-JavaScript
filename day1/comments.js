5382a1554f661e1c73f745af50d104f579fcb788
username <ivlevaarina@gmail.com>
2024-10-03 20:56:34 +0600

    task1-easy

---
+++ day1/comments.js
@@ -0,0 +1,10 @@
+//Task 1
+//comments can make code readable
+
+//Task 2
+//comments can make code readable, easy to reuse and informative
+
+//Task3
+/*
+  Welcome to 30DaysOfJavaScript
+*/
---
+++ day1/datatypes.js
@@ -0,0 +1,10 @@
+//Task 5
+let string = 'this is a string'
+let boolean = true
+let undefined
+let null0 = null
+
+console.log(typeof string)
+console.log(typeof boolean)
+console.log(typeof undefined)
+console.log(typeof null0)
