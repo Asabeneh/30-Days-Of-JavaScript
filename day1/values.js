cc07a9ac5c21b49f278acae4e7d933bfbb6f8ca5
username <ivlevaarina@gmail.com>
2024-10-03 20:20:43 +0600

    task1

---
+++ day1/values.js
@@ -0,0 +1,31 @@
+//Task 6
+let hello
+let a
+let hi
+let simple
+
+//Task 7
+let a1 = 'st'
+let js = 1
+let l = false
+let pr = 23.4
+
+//Task 8
+let firstName = 'Arina'
+let lastName = 'Ivleva'
+let country = 'Russia'
+let age = 16
+
+//Task 9
+//VS Code исправляет написание в одну строчку, так что решение записано в комментарии
+
+/*
+  let firstName2 = 'Arina'; let lastName2 = 'Ivleva'; let country2 = 'Russia'; let age2 = 16
+*/
+
+//Task 10
+let myAge = 'I am 25 years old'
+let yourAge = 'You are 30 years old.'
+
+console.log(myAge)
+console.log(yourAge)
---
+++ day1/variables.js
@@ -0,0 +1,5 @@
+//Task 4
+let string = 'this is a string'
+let boolean = true
+let undefined
+let null0 = null
