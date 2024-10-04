a6e8af24c87dee2a4fe084317d246d0029c0f554
username <ivlevaarina@gmail.com>
2024-10-04 15:37:44 +0600

    day2/task1-easy

---
+++ day2/Lvl1.js
@@ -0,0 +1,89 @@
+// Task 1
+
+let challenge = '30 Days Of JavaScript'
+
+//Task 2
+console.log(challenge)
+
+//Task 3
+console.log(challenge.length)
+
+//Task 4
+console.log(challenge.toUpperCase())
+
+//Task 5
+console.log(challenge.toLowerCase())
+
+//Task 6
+console.log(challenge.substring(0, 2))
+
+//Task 7
+console.log(challenge.substring(3, challenge.length))
+
+//Task 8
+console.log(challenge.includes('Script'))
+
+//Task 9
+console.log(challenge.split())
+
+//Task 10
+console.log(challenge.split(' '))
+
+//Task 11
+console.log(
+	'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon'.split(',')
+)
+
+//Task 12
+console.log(challenge.replace('JavaScript', 'Python'))
+
+//Task 13
+console.log(challenge.charAt(15))
+
+//Task 14
+console.log(challenge.charCodeAt(11))
+
+//Task 15
+console.log(challenge.indexOf('a'))
+
+//Task 16
+console.log(challenge.lastIndexOf('a'))
+
+//Task 17
+console.log(
+	'You cannot end a sentence with because because because is a conjunction'.indexOf(
+		'because'
+	)
+)
+
+//Task 18
+console.log(
+	'You cannot end a sentence with because because because is a conjunction'.lastIndexOf(
+		'because'
+	)
+)
+
+//Task 19
+console.log(
+	'You cannot end a sentence with because because because is a conjunction'.search(
+		'because'
+	)
+)
+
+//Task 20
+console.log(' 30 Days Of JavaScript '.trim())
+
+//Task 21
+console.log(challenge.startsWith('3'))
+
+//Task 22
+console.log(challenge.endsWith('t'))
+
+//Task 23
+console.log(challenge.match('a'))
+
+//Task 24
+console.log('30 Days Of '.concat('JavaScript'))
+
+//Task 25
+console.log(challenge.repeat(2))
