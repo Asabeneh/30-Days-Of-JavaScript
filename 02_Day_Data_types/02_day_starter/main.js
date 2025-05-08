87bf664b620ad06934a728e5dc637344814a8ac8
Frank Ojwang <fwillys40@gmail.com>
2025-05-09 02:36:24 +0300

    Update the first 4 days to master

--- 02_Day_Data_types/02_day_starter/main.js
+++ 02_Day_Data_types/02_day_starter/main.js
@@ -1,1 +1,94 @@
 // this is your main.js script
+let challenge = "30 Days Of Javascript";
+// console.log(challenge);
+// console.log(challenge.length);
+// console.log(challenge.toUpperCase());
+// console.log(challenge.toLowerCase());
+// console.log(challenge.substring(0, 2));
+// console.log(challenge.substring(3));
+// console.log(challenge.includes("Script"));
+// console.log(challenge.split());
+// console.log(challenge.split(" "));
+// console.log('Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon'.split(","));
+// console.log(challenge.replace("Javascript", "Python"));
+// console.log(challenge.charAt(15));
+// console.log(challenge.charCodeAt("J"));
+// console.log(challenge.indexOf("a"));
+// console.log(challenge.lastIndexOf("a"));
+// console.log(
+//   "You cannot end a sentence with because because because is a conjunction".indexOf("because")
+// );
+// console.log(
+//   "You cannot end a sentence with because because because is a conjunction".lastIndexOf(
+//     "because"
+//   )
+// );
+// console.log(
+//   "You cannot end a sentence with because because because is a conjunction".search(
+//     "because"
+//   )
+// );
+// console.log(" 30 Days Of Javascript ".trim(" "));
+// console.log(challenge.startsWith("30"));
+// console.log(challenge.endsWith("Javascript"));
+// console.log(challenge.match(/a/gi));
+// let challenge2 = "30 Days of";
+// let challenge3 = "Javascript";
+// console.log(challenge2.concat(" ", challenge3))
+// console.log(challenge.repeat(3))
+
+// console.log(`The quote 'There is no exercise better for the heart than reaching down and lifing people up.' by John Holmes teaches us to help one another.`)
+
+// console.log(`"Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead."
+// `);
+// console.log(parseFloat("10") === 10);
+// console.log("python".includes("on") && "jargon".includes("on"))
+// // console.log("I hope this course is not full of jargon".includes("jargon"))
+// console.log(`1\t1\t1\t1\t1\n2\t1\t2\t4\t8\n3\t1\t3\t9\t27\n4\t1\t4\t16\t64\n5\t1\t5\t25\t125`);
+// console.log(Math.floor(Math.random() * (100 - 50) + 50));
+// console.log(Math.floor(Math.random() * 256));
+// console.log("Javascript".charAt(Math.floor(Math.random() * "Javascript".length)));
+let text =
+  "You cannot end a sentence with because because because in a conjunction";
+// console.log(text.substring(text.indexOf("because"), text.lastIndexOf("because")));
+// console.log(text.lastIndexOf("because"));
+let text2 = text.split(" ");
+text2.splice(text2.indexOf("because"), 3);
+console.log(text2.join(" "));
+
+let loveText =
+  "Love is the best thing in this world. Some found their love and some are still looking for their love";
+
+let counter = 0;
+loveText.split(" ").forEach((element) => {
+  element.toLowerCase() === "love" ? counter++ : undefined;
+});
+console.log(counter);
+console.log(text.match(/because/gi).length);
+let income =
+  "He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.";
+let incomechunks = income.split(",");
+let totalIncome = 0;
+incomechunks.forEach((chunk) => {
+  if (chunk.includes("month")) {
+    chunk
+      .trim()
+      .split(" ")
+      .forEach((chunksalary) => {
+        if (+chunksalary > 0) {
+          totalIncome += chunksalary * 12;
+        }
+      });
+  }
+  if (chunk.includes("annual")) {
+    chunk
+      .trim()
+      .split(" ")
+      .forEach((chunksalary) => {
+        if (+chunksalary > 0) {
+          totalIncome += +chunksalary;
+        }
+      });
+  }
+});
+console.log(totalIncome);
--- 04_Day_Conditionals/04_day_starter/scripts/main.js
+++ 04_Day_Conditionals/04_day_starter/scripts/main.js
@@ -1,3 +1,122 @@
 // this is your main.js script
 
-alert('Open the browser console whenever you work on JavaScript')
+// alert('Open the browser console whenever you work on JavaScript')
+
+// let age = window.prompt("Enter your age");
+// let message =
+//   age >= 18
+//     ? `You are old enough to drive`
+//     : `You are left with ${18 - age} years to drive`;
+// window.alert(message);
+
+// let myAge = 82;
+// let yourAge = 32;
+
+// let comparison =
+//   myAge > yourAge
+//     ? `I am ${myAge - yourAge} years older than you`
+//     : `You are ${yourAge - myAge} years older than me`;
+// console.log(comparison);
+
+// let a = 21;
+// let b = 22;
+// const greater =
+//   a > b
+//     ? `${a} is greater than ${b}`
+//     : b > a
+//     ? `${b} is greater than ${a}`
+//     : `${a} is equal to ${b}`;
+// console.log(greater);
+
+// let number = 10;
+// const result = number % 2 == 0 ? `${number} is an even number` : `${number} is an odd number`;
+// console.log(result)
+
+// let score = window.prompt("Enter the score");
+// switch (true) {
+//   case score >= 80 && score <= 100:
+//     console.log("A");
+//     break;
+//   case score >= 70 && score < 80:
+//     console.log("B");
+//     break;
+//   case score >= 60 && score < 70:
+//     console.log("C");
+//     break;
+//   case score >= 50 && score < 60:
+//     console.log("D");
+//     break;
+//   default:
+//     console.log("F");
+//     break;
+// }
+
+// let period = window
+//   .prompt("Enter the month to find out the season")
+//   .toLowerCase();
+
+// switch (true) {
+//   case period == "september" || period == "october" || period == "november":
+//     console.log("The season is Autumn");
+//     break;
+//   case period == "december" || period == "january" || period == "february":
+//     console.log("The season is Winter");
+//     break;
+//   case period == "march" || period == "april" || period == "may":
+//     console.log("The season is Spring");
+//     break;
+//   default:
+//     console.log("The season is Summer");
+// }
+
+// let day = "friday".toLowerCase();
+
+// switch (true) {
+//     case day == "saturday" || day == "sunday":
+//         let processedWeekend = day.split("");
+//         processedWeekend.splice(0, 1);
+//         console.log(
+//           `${day.charAt(0).toUpperCase()}${processedWeekend.join(
+//             ""
+//           )} is a weekend`
+//         );
+//         break;
+//     default:
+//         let processedWeekDay = day.split("");
+//         processedWeekDay.splice(0, 1);
+//         console.log(
+//           `${day.charAt(0).toUpperCase()}${processedWeekDay.join(
+//             ""
+//           )} is a weekday`
+//         );
+
+// }
+
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+let daysNormal = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+let daysLeap = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+let month = "March";
+
+let currYear = new Date().getFullYear();
+let isLeap = currYear % 4 == 0 ? "leap" : "normal";
+
+console.log(
+  isLeap == "leap"
+    ? `The number of days in ${month} are ${daysLeap[months.indexOf(month)]}`
+    : `The number of days in ${month} are ${daysNormal[months.indexOf(month)]}`
+);
